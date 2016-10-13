406ee72100168b9367164e36bceeffd194a5107b
dibley1973 <dibley1973@yahoo.co.uk>
2016-10-13 06:54:05 +0100

    Added main context class

---
+++ src/Dibware.StoredProcedureFrameworkCore/StoredProcedureContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dibware.StoredProcedureFrameworkCore
+{
+    public class StoredProcedureContext
+    {
+    }
+}
