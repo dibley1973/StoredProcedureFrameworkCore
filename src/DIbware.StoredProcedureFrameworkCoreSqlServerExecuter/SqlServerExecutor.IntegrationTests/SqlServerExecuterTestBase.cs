0ac55fd8b308c1b43f80fc5fd32364bfb60d48b0
dibley1973 <dibley1973@yahoo.co.uk>
2016-11-09 05:19:52 +0000

    Started to add base class for integration tests

---
+++ src/DIbware.StoredProcedureFrameworkCoreSqlServerExecuter/SqlServerExecutor.IntegrationTests/SqlServerExecuterTestBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace Dibware.StoredProcedureFrameworkCore.SqlServerExecutor.IntegrationTests
+{
+    public class SqlServerExecuterTestBase
+    {
+        private string _connectionString;
+        private SqlConnection _connection;
+        //private TransactionScope _transaction;
+
+        protected SqlConnection Connection => _connection;
+
+        public SqlServerExecuterTestBase()
+        {
+
+        }
+    }
+}
