c95eec173496272aab152e26998d91118b0367fc
dibley1973 <dibley1973@yahoo.co.uk>
2016-11-16 06:39:43 +0000

    Further work on making basic stored procedure integration test pass

---
+++ src/Dibware.StoredProcedureFrameworkCore/StoredProcedureFrameworkCore/StoredProcedure{TResultSetType}.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dibware.StoredProcedureFrameworkCore
+{
+    public class StoredProcedure<TResultSetType> : StoredProcedure
+    {
+        private readonly IStoredProcedureExecutor _storedProcedureExecutor;
+
+        public StoredProcedure(IStoredProcedureExecutor storedProcedureExecutor)
+        {
+            if (storedProcedureExecutor == null) throw new ArgumentNullException(nameof(storedProcedureExecutor));
+
+            _storedProcedureExecutor = storedProcedureExecutor;
+        }
+
+        public TResultSetType Execute()
+        {
+            return _storedProcedureExecutor.ExecuteStoredProcedure(this);
+        }
+    }
+}
---
+++ src/Dibware.StoredProcedureFrameworkCore/StoredProcedureFrameworkCore/StoredProcedure{TReturn,TParameter}.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Dibware.StoredProcedureFrameworkCore.Exceptions;
+using Dibware.StoredProcedureFrameworkCore.Helpers;
+
+namespace Dibware.StoredProcedureFrameworkCore
+{
+    public class StoredProcedure<TResultSetType, TParameterType> : StoredProcedure
+    {
+        private readonly IStoredProcedureExecutor _storedProcedureExecutor;
+
+        public StoredProcedure(IStoredProcedureExecutor storedProcedureExecutor)
+        {
+            if (storedProcedureExecutor == null) throw new ArgumentNullException(nameof(storedProcedureExecutor));
+
+            _storedProcedureExecutor = storedProcedureExecutor;
+
+            SetProcedureName();
+        }
+
+        public TResultSetType ExecuteFor(TParameterType parameters)
+        {
+            return _storedProcedureExecutor.ExecuteStoredProcedureFor(this, parameters);
+        }
+
+        public string GetTwoPartName()
+        {
+            EnsureProcedureHasName();
+
+            return string.Format("{0}.{1}", SchemaName, ProcedureName);
+        }
+
+        private void EnsureProcedureHasName()
+        {
+            if (HasProcedureName()) return;
+
+            var message = "Stored procedure does not have a name set. Consider using " +
+                nameof(SetProcedureName) +
+                " before calling this method.";
+            throw ExceptionHelper.CreateStoredProcedureConstructionException(message);
+        }
+
+        /// <summary>
+        /// Ensures this instance is fully construcuted.
+        /// </summary>
+        /// <exception cref="StoredProcedureConstructionException">
+        /// this instance is not fully constrcuted
+        /// </exception>
+        public void EnsureIsFullyConstructed()
+        {
+            if (IsFullyConstructed()) return;
+
+            var message = ExceptionMessages.StoredProcedureIsNotFullyConstructed;
+            throw ExceptionHelper.CreateStoredProcedureConstructionException(message);
+        }
+
+        private bool HasReturnType()
+        {
+            return ReturnType != null;
+        }
+
+        /// <summary>
+        /// Determines if the procedure is fully constructed and in a valid 
+        /// state which can be called and executed
+        /// </summary>
+        /// <returns></returns>
+        public bool IsFullyConstructed()
+        {
+            return HasProcedureName() && HasReturnType();
+        }
+
+        /// <summary>
+        /// Gets the type of object to be returned as the result.
+        /// </summary>
+        /// <value>
+        /// The type of the result.
+        /// </value>
+        public Type ReturnType
+        {
+            get
+            {
+                return typeof(TResultSetType);
+            }
+        }
+    }
+}
